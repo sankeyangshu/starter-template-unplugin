424d93fe89e572352a0f8b5f81da2794d7b9e9b3
sankeyangshu <1260323835@qq.com>
2025-08-14 08:47:49 +0800

    feat: init unplugin

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import { createUnplugin, type UnpluginInstance } from 'unplugin';
+import { createFilter } from 'unplugin-utils';
+import { resolveOptions, type Options } from './core/options';
+
+export const Starter: UnpluginInstance<Options | undefined, false> = createUnplugin((rawOptions = {}) => {
+  const options = resolveOptions(rawOptions);
+  const filter = createFilter(options.include, options.exclude);
+
+  const name = 'unplugin-starter';
+
+  return {
+    name,
+    enforce: options.enforce,
+
+    transformInclude(id) {
+      return filter(id);
+    },
+
+    transform(code, _id) {
+      return `// unplugin-starter injected\n${code}`;
+    },
+  };
+});
