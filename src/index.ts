2b1ff616442573fcda40e8b81740d25dafc7bbdb
sankeyangshu <1260323835@qq.com>
2026-09-17 09:10:15 +0800

    refactor: improve code formatting and structure in index.ts for better readability

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,28 @@
 import type { UnpluginInstance } from 'unplugin';
-import type { Options } from './core/options';
 import { createUnplugin } from 'unplugin';
 import { createFilter } from 'unplugin-utils';
+
+import type { Options } from './core/options';
 import { resolveOptions } from './core/options';
 
-export const Starter: UnpluginInstance<Options | undefined, false> = createUnplugin((rawOptions = {}) => {
-  const options = resolveOptions(rawOptions);
-  const filter = createFilter(options.include, options.exclude);
+export const Starter: UnpluginInstance<Options | undefined, false> = createUnplugin(
+  (rawOptions = {}) => {
+    const options = resolveOptions(rawOptions);
+    const filter = createFilter(options.include, options.exclude);
 
-  const name = 'unplugin-starter';
+    const name = 'unplugin-starter';
 
-  return {
-    name,
-    enforce: options.enforce,
+    return {
+      name,
+      enforce: options.enforce,
 
-    transformInclude(id) {
-      return filter(id);
-    },
+      transformInclude(id) {
+        return filter(id);
+      },
 
-    transform(code, _id) {
-      return `// unplugin-starter injected\n${code}`;
-    },
-  };
-});
+      transform(code, _id) {
+        return `// unplugin-starter injected\n${code}`;
+      },
+    };
+  }
+);
